2b7d840bf2dbb9b93a12298e5672c3f6fef48c25
Sachitha Hirushan <shpsachitha@gmail.com>
2021-11-09 08:39:04 +0530

    Custom err wip

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,7 +1,9 @@
 import { ValidationError } from 'express-validator';
+import {CustomError} from './custom-error';
 
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError  {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
     super('Invalid request parameters');
 
@@ -10,7 +12,14 @@ export class RequestValidationError extends Error {
   }
 
 
-  
+  serializeErrors(){
+    return this.errors.map(err => {
+      return {
+        message: err.msg,
+        field: err.param
+      }
+    });
+  }
   
 }
 
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { Request,Response,NextFunction } from "express";
-
-import {RequestValidationError} from '../errors/request-validation-error';
-import {DatabaseConnectionError} from '../errors/database-connection-error';
+import {CustomError} from "../errors/custom-error";
 
 export const errorHandler = (
     err: Error,
@@ -9,21 +7,11 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
     ) => {
-        if(err instanceof RequestValidationError){
+        if(err instanceof CustomError){
             console.log('RequestValidationError');
-            const formatedErrors = err.errors.map(error => {
-                return { message: error.msg, field: error.param };
+            return res.status(err.statusCode).json({
+                errors: err.serializeErrors()
             });
-
-            return res.status(400).json({
-                errors: formatedErrors
-            });
-        }
-        if(err instanceof DatabaseConnectionError){
-            console.log('DatabaseConnectionError');
-            return res.status(500).send({errors: [
-                {message: err.reason}
-            ]});
         }
         console.log(err);
         res.status(400).send({errors: [
