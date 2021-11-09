0a2f1ca013ae86a0e398f2f597354e2a143d2b7d
Sachitha Hirushan <shpsachitha@gmail.com>
2021-11-09 23:21:42 +0530

    JWT init done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json} from 'body-parser';
-
+import cookieSession from 'cookie-session';
 import {currentUserRouter} from './routes/current-user';
 import {signupRouter} from './routes/signup';
 import {signoutRouter} from './routes/signout';
@@ -11,7 +11,17 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
 const app = express();
+app.set('trust proxy',true);
 app.use(json());
+
+app.use(
+    cookieSession(
+        {
+            signed: false,
+            secure: true
+        }
+    )
+)
 
 app.use(currentUserRouter);
 app.use(signupRouter);
