6e73420936bc746230968a0968cdae56872d7372
Sachitha Hirushan <shpsachitha@gmail.com>
2021-11-16 20:09:26 +0530

    wip

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,13 @@
+import { Request,Response,NextFunction } from "express";
+import {NotAuthError} from "../errors/not-auth-error";
+
+export const requireAuth = (
+    req:Request, 
+    res: Response,
+    next:NextFunction
+    ) =>{
+        if(!req.currentUser){
+            throw new NotAuthError();
+        }
+        next();
+};
