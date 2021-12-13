68923d89ab2eb68aa7c325479442ac115f8745a9
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-13 19:39:02 +0530

    wip npm package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import {signupRouter} from './routes/signup';
 import {signoutRouter} from './routes/signout';
 import {signinRouter} from './routes/signin';
 
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { errorHandler,NotFoundError } from '@infiniteam/common';
 
 const app = express();
 app.set('trust proxy',true);
