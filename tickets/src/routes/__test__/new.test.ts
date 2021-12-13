8c5b769297a152a2d948beb682a93dd13b9ecb68
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-13 21:59:05 +0530

    tickets test wip

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest';
+import {app} from '../../../src/app';
+
+
+it('has a route handle listenning to /api/tickets for post requests',async () => {
+    const response = await request(app)
+                    .post('/api/tickets')
+                    .send({});
+
+    expect(response.status).not.toEqual(404);
+})
+
+it('can only be accesses if the user is signed in',async () => {
+    
+})
+
+it('returns an error if an invalid title is provided',async () => {
+    
+})
+
+it('returns an error if invalid price is provided',async () => {
+    
+})
+
+it('creates a ticket with valid inputs',async () => {
+    
+})
