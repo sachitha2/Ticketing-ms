302dbcf9596fbdafbc792d9add7af3eb8cce463d
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-13 12:40:22 +0530

    wip

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,6 @@ import axios from "axios";
 export default ({req})=>{
     if(typeof window === 'undefined'){
         //server
-
         return axios.create({
             baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
             headers: req.headers
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import buildClient from '../api/build-client';
 const  LandingPage = ({currentUser}) => {
-    console.log('current user is '+currentUser)
     return currentUser ? (
         <h1>Welcome to the app </h1>
     ):(
@@ -12,7 +11,6 @@ const  LandingPage = ({currentUser}) => {
 LandingPage.getInitialProps = async context => {
     const client = buildClient(context);
     const { data } = await client.get('/api/users/currentuser/');
-    console.log(data)
     return data;
 };
 
