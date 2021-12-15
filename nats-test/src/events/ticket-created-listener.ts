125e160f325cf088ea600b0675a88964a457d302
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-16 00:01:41 +0530

    wip

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,15 +1,17 @@
 import {Message} from 'node-nats-streaming';
 import {Listener} from './base-listener';
+import {TicketCreatedEvent} from './ticket-created-event';
+import {Subjects} from './subjects';
 
-export class TicketCreatedListener extends Listener{
-    subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
 
-    onMessage(data: any,msg: Message){
+    onMessage(data: TicketCreatedEvent['data'],msg: Message){
         console.log(data);
         console.log(`Processing the order...`);
-        
+        console.log(data.id);
         msg.ack();
        
     }
