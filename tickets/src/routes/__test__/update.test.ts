2527f3eb0c66816f129675cb377de42296e5a21b
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-18 22:46:44 +0530

    nats update done

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import {app} from '../../../src/app';
 import mongoose from 'mongoose';
+import {natsWrapper} from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
     const id =new mongoose.Types.ObjectId().toHexString();
@@ -99,3 +100,25 @@ it('updates the ticket provided valid inputs', async () => {
     expect(ticketResponse.body.title).toEqual('sdg');
     expect(ticketResponse.body.price).toEqual(1000);
 })
+
+it('publishes an event', async ()=>{
+    const cookie = global.signin();
+    const response = await request(app)
+            .post('/api/tickets')
+            .set('Cookie', cookie)
+            .send({
+                title: 'sdg',
+                price: 20
+            })
+    await request(app)
+            .put(`/api/tickets/${response.body.id}`)
+            .set('Cookie',cookie)
+            .send({
+                title: 'hfeuhgu',
+                price: 10
+            })
+            .expect(200);
+    // console.log('natsWrapper.client.publish',natsWrapper.client.publish);
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+    
+});
